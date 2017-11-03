e54af59c609408118767055a42db100ca9ce444a
Robert Dimitrov <robert_dimitrov@me.com>
2017-11-03 21:20:50 +0200

    - Added functionality to keep the selected image centered inside the crop area

--- PixelGram/Classes/Screens/Camera/Crop/CropImageViewController.swift
+++ PixelGram/Classes/Screens/Camera/Crop/CropImageViewController.swift
@@ -47,6 +47,36 @@ class CropImageViewController: ViewController {
 
     // MARK: Image display
     
+    func imageRect() -> CGRect {
+        guard let image = image, let imageView = imageView else {
+            return CGRect.zero
+        }
+        
+        let imageSize = image.size
+        let imageSizeRatio: CGFloat = imageSize.width / imageSize.height
+        
+        let width = imageView.frame.width
+        let height = imageView.frame.height
+        
+        if imageSizeRatio > 1 {
+            // horizontal lines
+            
+            let size = CGSize(width: width, height: width * (1.0 / imageSizeRatio))
+            
+            return CGRect(origin: CGPoint(x: 0, y: (height / 2.0) - (size.height / 2.0)),
+                          size: size)
+        } else if imageSizeRatio < 1 {
+            // vertical lines
+         
+            let size = CGSize(width: height * imageSizeRatio, height: height)
+            
+            return CGRect(origin: CGPoint(x: (width / 2.0) - (size.width / 2.0), y: 0),
+                          size: size)
+        }
+        
+        return imageView.frame
+    }
+    
     var targetSize: CGSize {
         let scale = UIScreen.main.scale
         return CGSize(width: (imageView?.bounds.width ?? 0) * scale,
@@ -111,8 +141,64 @@ extension CropImageViewController: UIScrollViewDelegate {
         return imageView
     }
     
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print(scrollView.contentOffset)
+    func centerOrPositionImageContentOffset(scrollView: UIScrollView, targetContentOffset: CGPoint? = nil) -> CGPoint {
+        
+        let imageFrame = imageRect()
+        let visibleFrame = visibleRect(scrollView: scrollView)
+        
+        var contentOffset = targetContentOffset ?? scrollView.contentOffset
+        
+        // if image can fit but not centered
+        
+        if imageFrame.height <= visibleFrame.height {
+            let spacing = (visibleFrame.height - imageFrame.height) / 2.0
+            
+            contentOffset = CGPoint(x: scrollView.contentOffset.x, y: imageFrame.minY - spacing)
+        } else if imageFrame.width <= visibleFrame.width {
+            let spacing = (visibleFrame.width - imageFrame.width) / 2.0
+            
+            contentOffset = CGPoint(x: imageFrame.minX - spacing, y: scrollView.contentOffset.x)
+        } else {
+            // else if outside bounds
+            
+            if visibleFrame.minX < imageFrame.minX {
+                contentOffset.x = imageFrame.minX
+            } else if visibleFrame.maxX > imageFrame.maxX {
+                contentOffset.x = imageFrame.maxX - visibleFrame.width
+            }
+            
+            if visibleFrame.minY < imageFrame.minY {
+                contentOffset.y = imageFrame.minY
+            } else if visibleFrame.maxY > imageFrame.maxY {
+                contentOffset.y = imageFrame.maxY - visibleFrame.height
+            }
+        }
+        
+        return contentOffset
+    }
+    
+    func visibleRect(scrollView: UIScrollView) -> CGRect {
+        let origin = scrollView.contentOffset
+        let size = scrollView.bounds.size
+        
+        return CGRect(origin: origin, size: size)
+    }
+    
+    func scrollImageToVisible(scrollView: UIScrollView) {
+        let contentOffset = centerOrPositionImageContentOffset(scrollView: scrollView)
+        scrollView.setContentOffset(contentOffset, animated: true)
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        scrollImageToVisible(scrollView: scrollView)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        scrollImageToVisible(scrollView: scrollView)
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        scrollImageToVisible(scrollView: scrollView)
     }
     
 }
